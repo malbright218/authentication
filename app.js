62e51f38c06ef518136271633388d4fc31bf784f
Mark Albright <malbright@MarkAlbrights-Mac-mini.local>
2019-06-26 16:55:54 -0400

    initial upload

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+const flash = require("connect-flash");
+const session = require("express-session");
+const passport = require('passport')
+
+const app = express();
+
+// PASSPORT CONFIG
+require('./config/passport')(passport)
+
+//DB CONFIG
+const db = require("./config/keys").MongoURI;
+
+// MONGO CONNECTION
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("DB connected"))
+  .catch(err => console.log(err));
+
+// EJS MIDDLEWARE
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+
+// BODYPARSER
+app.use(express.urlencoded({ extended: false }));
+
+// EXPRESS SESSION MIDDLEWARE
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// PASSPORT MIDDLEWARE
+app.use(passport.initialize());
+app.use(passport.session());
+
+// CONNECT FLASH
+app.use(flash());
+
+// GLOBAL VARIABLES
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
+});
+
+// ROUTES
+app.use("/", require("./routes/index"));
+app.use("/users", require("./routes/users"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
